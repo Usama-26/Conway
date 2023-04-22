7daeaf99696e1576de6739e9725f0a3b99fc1a6e
Usama Ali <75754522+Usama-26@users.noreply.github.com>
2023-04-22 10:22:53 +0500

    Extended Product Cart Fixed

--- src/components/ProductCardExtended/index.tsx
+++ src/components/ProductCardExtended/index.tsx
@@ -13,10 +13,12 @@ export default function ProductCardExtended({
   return (
     <div className="flex flex-col gap-y-1">
       <div className=" transition-transform duration-100 hover:bg-[#EEEAEA] p-2 rounded-md ">
-        <div className="w-full flex md:flex-row flex-col justify-between items-center rounded-md py-5 px-4 pr-0 font-poppins text-black border bg-white cursor-pointer ">
+        <div className=" flex md:flex-row flex-col justify-between items-center rounded-md h-[285px] py-5 px-4 pr-0 font-poppins text-black border bg-white cursor-pointer ">
           <div className="md:basis-3/5 basis-full md:order-1 order-2 md:py-0 py-4">
-            <h1 className="md:text-2xl text-xl font-bold">{product.title}</h1>
-            <div className="my-2">
+            <h1 className="md:text-2xl h-[54px] text-xl font-bold">
+              {product.title}
+            </h1>
+            <div className="my-2 h-[46px]">
               {product?.product_identifiers?.map(identifier => (
                 <Suspense key={identifier.id}>
                   <span>
@@ -25,14 +27,15 @@ export default function ProductCardExtended({
                   <br />
                 </Suspense>
               ))}
-
               <span>{getCompareTo(product)}</span>
             </div>
-            <p className="mb-8 md:text-base text-sm font-poppins">
-              {product.short_description}
+            <p className=" md:text-base my-8 text-sm font-poppins">
+              {product?.short_description?.length > 200
+                ? product.short_description.substring(0, 150).concat('...')
+                : product.short_description}
             </p>
             <div className="flex gap-3 font-poppins">
-              <button className="px-5 py-1 bg-[#5B5B5B] rounded-lg font-medium border border-transparent text-white hover:text-black sm:text-base text-sm hover:border-black hover:bg-white transition duration-200 ">
+              <button className="px-5 py-1 bg-[#5B5B5B] rounded-lg font-medium border border-transparent text-white hover:text-black sm:text-base text-sm hover:border-black hover:bg-white transition duration-200">
                 Learn More
               </button>
               <button className="px-5 py-1 text-black rounded-lg font-medium border border-transparent bg-yellow-700 hover:border-black sm:text-base text-sm hover:bg-white transition duration-200 ">
@@ -41,12 +44,12 @@ export default function ProductCardExtended({
             </div>
           </div>
           <div className="md:basis-2/5 basis-full md:order-2 order-1 md:py-0 py-4">
-            <div className="w-[300px] h-[172px] relative">
+            <div className="w-[424px] h-[285px] relative">
               <Image
                 src={getProductImage(product)}
                 fill
                 className="
-                object-cover"
+                object-contain"
                 alt={product.default_image?.description || ''}
                 // className={'h-full object-contain'}
               />
